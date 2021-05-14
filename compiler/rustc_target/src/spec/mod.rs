38a374b345c8247898ec94ad48405ff2a789f869
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-05-13 23:33:05 -0400

    Add aarch64-skyline-switch target to rustc

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -897,6 +897,8 @@ supported_targets! {
     ("aarch64_be-unknown-linux-gnu", aarch64_be_unknown_linux_gnu),
     ("aarch64-unknown-linux-gnu_ilp32", aarch64_unknown_linux_gnu_ilp32),
     ("aarch64_be-unknown-linux-gnu_ilp32", aarch64_be_unknown_linux_gnu_ilp32),
+
+    ("aarch64-skyline-switch", aarch64_skyline_switch),
 }
 
 /// Everything `rustc` knows about how to compile for a specific target.
